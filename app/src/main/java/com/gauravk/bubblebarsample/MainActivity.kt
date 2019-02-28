238897ef28c763feccf3e945d5db0b02855e1d3b
gaurav <19gauravk95@gmail.com>
2019-03-01 02:03:38 +0530

    Update Sample

--- app/src/main/java/com/gauravk/bubblebarsample/MainActivity.kt
+++ app/src/main/java/com/gauravk/bubblebarsample/MainActivity.kt
@@ -2,8 +2,8 @@ package com.gauravk.bubblebarsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.gauravk.bubblenavigation.BubbleNavigationLinearView
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val view = findViewById<BubbleNavigationLinearView>(R.id.bottom_navigation_view_linear)
-        view.setNavigationChangeListener { _, position ->
-            Log.i("Main", "Nav Changed at $position")
+        //listeners for top navigation view
+        top_navigation_constraint_text.text = String.format(
+            getString(R.string.position_placeholder_top),
+            top_navigation_constraint.currentActiveItemPosition
+        )
+        top_navigation_constraint.setNavigationChangeListener { _, position ->
+            top_navigation_constraint_text.text = String.format(getString(R.string.position_placeholder_top), position)
+        }
+
+        //listener for bottom navigation view
+        bottom_navigation_linear.text = String.format(
+            getString(R.string.position_placeholder_bottom),
+            bottom_navigation_view_linear.currentActiveItemPosition
+        )
+        bottom_navigation_view_linear.setNavigationChangeListener { _, position ->
+            bottom_navigation_linear.text = String.format(getString(R.string.position_placeholder_bottom), position)
         }
     }
 }
