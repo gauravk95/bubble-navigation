b97eaacdb394f46c0e1089f8d6aa9338a6f08e01
S Tanveer Hussain <tanveer1807@yahoo.com>
2019-06-19 23:50:57 +0500

    Update BubbleNavigationLinearView.java

--- bubblenavigation/src/main/java/com/gauravk/bubblenavigation/BubbleNavigationLinearView.java
+++ bubblenavigation/src/main/java/com/gauravk/bubblenavigation/BubbleNavigationLinearView.java
@@ -37,198 +37,115 @@ import java.util.ArrayList;
 @SuppressWarnings("unused")
 public class BubbleNavigationLinearView extends LinearLayout implements View.OnClickListener {
 
-    //constants
-    private static final String TAG = "BNLView";
-    private static final int MIN_ITEMS = 2;
-    private static final int MAX_ITEMS = 5;
-
-    private ArrayList<BubbleToggleView> bubbleNavItems;
-    private BubbleNavigationChangeListener navigationChangeListener;
-
+    private String CURRENT_ACTIVE_VIEW = "x.f";
     private int currentActiveItemPosition = 0;
+    private static final String TAG = "bnlview";
+    private BubbleNavigationChangeListener navigationChangeListener = null;
 
-    /**
-     * Constructors
-     */
-    public BubbleNavigationLinearView(@NonNull Context context) {
+    public BubbleNavigationLinearView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        init();
+
+    }
+    public BubbleNavigationLinearView(Context context) {
         super(context);
-        init(context, null);
+        init();
     }
 
-    public BubbleNavigationLinearView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init(context, attrs);
-    }
-
-    public BubbleNavigationLinearView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public BubbleNavigationLinearView(Context context, AttributeSet attrs, int defStyleAttr)  {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init();
     }
 
-    @Override
-    protected Parcelable onSaveInstanceState() {
-        Bundle bundle = new Bundle();
-        bundle.putParcelable("superState", super.onSaveInstanceState());
-        bundle.putInt("current_item", currentActiveItemPosition);
-        return bundle;
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Parcelable state) {
-        if (state instanceof Bundle) {
-            Bundle bundle = (Bundle) state;
-            currentActiveItemPosition = bundle.getInt("current_item");
-            state = bundle.getParcelable("superState");
-        }
-        super.onRestoreInstanceState(state);
-    }
-
-    /////////////////////////////////////////
-    // PRIVATE METHODS
-    /////////////////////////////////////////
-
-    /**
-     * Initialize
-     *
-     * @param context current context
-     * @param attrs   custom attributes
-     */
-    private void init(Context context, AttributeSet attrs) {
-
+    public void init() {
         setOrientation(HORIZONTAL);
         setGravity(Gravity.CENTER);
-
-        post(new Runnable() {
-            @Override
-            public void run() {
-                updateChildNavItems();
-            }
-        });
     }
 
-    /**
-     * Finds Child Elements of type {@link BubbleToggleView} and adds them to {@link #bubbleNavItems}
-     */
-    private void updateChildNavItems() {
-        bubbleNavItems = new ArrayList<>();
-        for (int index = 0; index < getChildCount(); ++index) {
-            View view = getChildAt(index);
-            if (view instanceof BubbleToggleView)
-                bubbleNavItems.add((BubbleToggleView) view);
-            else {
-                Log.w(TAG, "Cannot have child bubbleNavItems other than BubbleToggleView");
-                return;
-            }
-        }
-
-        if (bubbleNavItems.size() < MIN_ITEMS) {
-            Log.w(TAG, "The bubbleNavItems list should have at least 2 bubbleNavItems of BubbleToggleView");
-        } else if (bubbleNavItems.size() > MAX_ITEMS) {
-            Log.w(TAG, "The bubbleNavItems list should not have more than 5 bubbleNavItems of BubbleToggleView");
-        }
-
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        checkViewsAreAllBubble();
         setClickListenerForItems();
-        setInitialActiveState();
+        refreshActiveState();
         updateMeasurementForItems();
     }
 
-    /**
-     * Makes sure that ONLY ONE child {@link #bubbleNavItems} is active
-     */
-    private void setInitialActiveState() {
+    private void refreshActiveState() {
         boolean foundActiveElement = false;
-        for (int i = 0; i < bubbleNavItems.size(); i++) {
-            if (bubbleNavItems.get(i).isActive() && !foundActiveElement) {
+        for (int i=0;i<this.getChildCount();++i) {
+            BubbleToggleView btv = (BubbleToggleView)getChildAt(i);
+            if (btv.isActive() && !foundActiveElement) {
                 foundActiveElement = true;
-                currentActiveItemPosition = i;
+                this.currentActiveItemPosition = i;
             } else {
-                bubbleNavItems.get(i).setInitialState(false);
+                btv.setInitialState(false);
             }
         }
-
         if (!foundActiveElement) {
-            bubbleNavItems.get(currentActiveItemPosition).setInitialState(true);
+            ((BubbleToggleView)getChildAt(currentActiveItemPosition)).setInitialState(true);
         }
     }
 
-    /**
-     * Update the measurements of the child components {@link #bubbleNavItems}
-     */
     private void updateMeasurementForItems() {
-        int numChildElements = bubbleNavItems.size();
-        if (numChildElements > 0) {
-            int calculatedEachItemWidth = (getMeasuredWidth() - (getPaddingRight() + getPaddingLeft())) / numChildElements;
-            for (BubbleToggleView btv : bubbleNavItems)
-                btv.updateMeasurements(calculatedEachItemWidth);
+        int  numChildViews = getChildCount();
+        if(numChildViews > 0) {
+            int calculatedEachItemWidth = (getMeasuredWidth() - (getPaddingRight() + getPaddingLeft())) / numChildViews;
+            for(int i=0;i<numChildViews;++i) {
+                ((BubbleToggleView)getChildAt(i)).updateMeasurements(calculatedEachItemWidth);
+            }
         }
     }
 
-    /**
-     * Sets {@link android.view.View.OnClickListener} for the child views
-     */
-    private void setClickListenerForItems() {
-        for (BubbleToggleView btv : bubbleNavItems)
-            btv.setOnClickListener(this);
+    private void checkViewsAreAllBubble() {
+        for( int i=0;i<getChildCount();++i) {
+            View view = this.getChildAt(i);
+            final boolean wrongView = !(view instanceof BubbleToggleView);
+            if(wrongView) {
+                throw new IllegalStateException("only BubbleToggles are allowed");
+            }
+        }
     }
 
-    /**
-     * Gets the Position of the Child from {@link #bubbleNavItems} from its id
-     *
-     * @param id of view to be searched
-     * @return position of the Item
-     */
+
+    private void setClickListenerForItems() {
+        for(int i=0;i<getChildCount();++i) {
+            ((BubbleToggleView)getChildAt(i)).setOnClickListener(this);
+        }
+    }
+
     private int getItemPositionById(int id) {
-        for (int i = 0; i < bubbleNavItems.size(); i++)
-            if (id == bubbleNavItems.get(i).getId())
+        for(int i=0;i<getChildCount();++i) {
+            if(id == this.getChildAt(i).getId()) {
                 return i;
+            }
+        }
         return -1;
     }
 
-    ///////////////////////////////////////////
-    // PUBLIC METHODS
-    ///////////////////////////////////////////
-
-    /**
-     * Set the navigation change listener {@link BubbleNavigationChangeListener}
-     *
-     * @param navigationChangeListener sets the passed parameters as listener
-     */
-    public void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener) {
-        this.navigationChangeListener = navigationChangeListener;
+    public void setNavigationChangeListener(BubbleNavigationChangeListener listener) {
+        this.navigationChangeListener = listener;
+        this.navigationChangeListener.onNavigationChanged(this.getChildAt(currentActiveItemPosition),this.currentActiveItemPosition);
     }
 
-    /**
-     * Set the {@link Typeface} for the Text Elements of the View
-     *
-     * @param typeface to be used
-     */
     public void setTypeface(Typeface typeface) {
-        for (BubbleToggleView btv : bubbleNavItems)
-            btv.setTitleTypeface(typeface);
+        for(int i=0;i<getChildCount();++i) {
+            final BubbleToggleView view = (BubbleToggleView)getChildAt(i);
+            view.setTitleTypeface(typeface);
+        }
     }
 
-    /**
-     * Gets the current active position
-     *
-     * @return active item position
-     */
     public int getCurrentActiveItemPosition() {
         return currentActiveItemPosition;
     }
 
-    /**
-     * Sets the current active item
-     *
-     * @param position current position change
-     */
     public void setCurrentActiveItem(int position) {
-        if (currentActiveItemPosition == position) return;
-        if (position < 0 || position >= bubbleNavItems.size())
-            return;
-        BubbleToggleView btv = bubbleNavItems.get(position);
+        if(currentActiveItemPosition == position) return;
+        if(position < 0 || position >= this.getChildCount()) {
+            return ;
+        }
+        BubbleToggleView btv = (BubbleToggleView)this.getChildAt(position);
         btv.performClick();
     }
-
     @Override
     public void onClick(View v) {
         int changedPosition = getItemPositionById(v.getId());
@@ -236,20 +153,24 @@ public class BubbleNavigationLinearView extends LinearLayout implements View.OnC
             if (changedPosition == currentActiveItemPosition) {
                 return;
             }
-            BubbleToggleView currentActiveToggleView = bubbleNavItems.get(currentActiveItemPosition);
-            BubbleToggleView newActiveToggleView = bubbleNavItems.get(changedPosition);
-            if (currentActiveToggleView != null)
-                currentActiveToggleView.toggle();
-            if (newActiveToggleView != null)
-                newActiveToggleView.toggle();
-
+            BubbleToggleView currentActiveToggleView = (BubbleToggleView)this.getChildAt(currentActiveItemPosition);
+            BubbleToggleView newActiveToggleView = (BubbleToggleView)this.getChildAt(changedPosition);
+            currentActiveToggleView.toggle();
+            newActiveToggleView.toggle();
             //changed the current active position
             currentActiveItemPosition = changedPosition;
-
-            if (navigationChangeListener != null)
-                navigationChangeListener.onNavigationChanged(v, currentActiveItemPosition);
+            navigationChangeListener.onNavigationChanged(v, currentActiveItemPosition);
         } else {
             Log.w(TAG, "Selected id not found! Cannot toggle");
         }
+    }
+
+    public void onSaveInstanceState(Bundle bundle) {
+        bundle.putInt(CURRENT_ACTIVE_VIEW,currentActiveItemPosition);
+    }
+
+    public void onRestoreInstanceState(Bundle bundle) {
+        int currentActiveView = bundle.getInt(CURRENT_ACTIVE_VIEW,0);
+        setCurrentActiveItem(currentActiveView);
     }
 }
